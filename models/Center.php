17c4a30ace89c9d5f2702b0c777f72aa3b40e541
alfredo <alfredofauramolina@gmail.com>
2019-03-24 15:42:53 +0100

    Se añaden los archivos del nuevo modelo basado en la tabla center de la bd.

---
+++ models/Center.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "center".
+ *
+ * @property int $id
+ * @property string $nombre
+ * @property string $direccion
+ * @property string $poblacion
+ * @property string $provincia
+ * @property string $centerCode
+ */
+class Center extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'center';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre', 'direccion', 'poblacion', 'provincia', 'centerCode'], 'required'],
+            [['nombre'], 'string', 'max' => 100],
+            [['direccion'], 'string', 'max' => 200],
+            [['poblacion', 'provincia'], 'string', 'max' => 50],
+            [['centerCode'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'direccion' => 'Direccion',
+            'poblacion' => 'Poblacion',
+            'provincia' => 'Provincia',
+            'centerCode' => 'Center Code',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return CenterQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new CenterQuery(get_called_class());
+    }
+}
---
+++ views/center/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Center */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="center-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'poblacion')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'provincia')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'centerCode')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
