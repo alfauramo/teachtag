dc3f54032a95c4c94b2ad0b2004b5f6631190478
alfauramo <alfredofauramolina@gmail.com>
2019-04-02 18:43:26 +0200

    Se añaden los archivos modificados.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,14 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\User;
+use app\models\VerificarCorreoForm;
+use app\models\ComprobarCorreoForm;
+use app\models\ChangePasswordForm;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
 
 class SiteController extends BaseController
 {
@@ -124,6 +132,82 @@ class SiteController extends BaseController
     public function actionAbout()
     {
         return $this->render('about');
+    }
+
+    /**
+     * Verifica que la cuenta de correo del usuario exista
+     */
+    public function actionComprobarCorreo()
+    {
+        $model = new ComprobarCorreoForm();
+
+        return $this->render('comprobar_correo', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionVerificarCorreo()
+    {
+
+        $correo = new ComprobarCorreoForm();
+        $correo->load(Yii::$app->request->post());
+
+        $user = User::find()->where(['email' => $correo])->one();
+
+        if($user === NULL){
+            return $this->render('bad_email');
+        }else{
+           
+            $msg = "<p>Ha solicitado un cambio de contraseña. Por favor, haga click en el siguiente enlace para verificar el cambio: <a href='";
+            $msg .= Url::base(true);
+            $msg .= Url::to(['site/confirmar-recuperacion', 'id' => $user->id, 'token' => $user->accessToken]);
+            $msg .= "'>Enlace</a>";
+
+            Yii::$app->mailer->compose()
+                ->setFrom(Yii::$app->params["adminEmail"])
+                ->setTo($correo->email)
+                ->setSubject('Nueva contraseña')
+                ->setHtmlBody($msg)
+                ->send();
+
+            return $this->render('good_email');
+            
+        }
+        
+    }
+
+    public function actionConfirmarRecuperacion($id, $token){
+        $model = User::find()->where(['id' => $id])->one();
+        
+        if($model === NULL){
+            throw new NotFoundHttpException('Datos proporcionados no válidos');
+        }else{
+            if($model->accessToken !== $token){
+                throw new NotFoundHttpException('Datos proporcionados no válidos');
+            }
+            $cPModel = new ChangePasswordForm();
+            $cPModel->email = $model->email;
+
+            return $this->render('changepass', [
+                'model' => $cPModel,
+            ]);
+        }
+    }
+
+    public function actionChangePassword($email) {
+        $cPModel = new ChangePasswordForm();
+        $cPModel->load(Yii::$app->request->post());
+
+        $model = User::find()->where(['email' => $email])->one();
+
+        if($model !== NULL){
+            $model->password = $cPModel->password;
+            $model->password = crypt($model->password, Yii::$app->params["salt"]);  
+            if ($model->save()){
+                return $this->render('cambio_pass_ok');
+            }
+        }
+        return $this->render('cambio_pass_ko');
     }
 
     
---
+++ models/ChangePasswordForm.php
@@ -0,0 +1,29 @@
+<?php
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+
+class ChangePasswordForm extends Model
+{
+	public $password;
+	public $password_repeat;
+	public $email;
+	public function rules() {
+		return [
+			['email', 'email'],
+	        [['password', 'password_repeat'], 'required'],
+	        ['password', 'string', 'min' => 6],
+	        ['password_repeat', 'compare', 'compareAttribute'=>'password', 'message'=>"Las contraseñas no coinciden" ],
+        ];
+	}
+
+	public function attributeLabels()
+	{
+		return [
+			'password' => 'Contraseña',
+			'password_repeat' => 'Repita contraseña',
+		];
+	}
+}
