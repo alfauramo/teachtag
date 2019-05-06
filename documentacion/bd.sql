8941a09ef211aaae81c58a9d918fbfe510fd5cf5
alfredo <alfredofauramolina@gmail.com>
2019-05-06 14:36:37 +0200

    Adjunto bd actualizada

--- documentacion/bd.sql
+++ documentacion/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2019 a las 13:39:13
+-- Tiempo de generación: 06-05-2019 a las 14:35:46
 -- Versión del servidor: 10.1.35-MariaDB
 -- Versión de PHP: 7.2.9
 
@@ -44,6 +44,19 @@ CREATE TABLE `center` (
 
 INSERT INTO `center` (`id`, `nombre`, `poblacion`, `provincia`, `centerCode`) VALUES
 (2, 'IES Macià Abela', 'Crevillent', 'Alacant', 'a1b2c3d4');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos`
+--
+
+CREATE TABLE `fotos` (
+  `id` int(11) NOT NULL,
+  `ruta` varchar(255) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -91,16 +104,28 @@ CREATE TABLE `user` (
   `twitter` varchar(100) NOT NULL,
   `hobbies` text,
   `films` text,
-  `music` text
+  `music` text,
+  `privado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `rol`, `name`, `email`, `birthday`, `descripcion`, `authKey`, `accessToken`, `activate`, `centerCode`, `img_perfil`, `img_cabecera`, `facebook`, `twitter`, `hobbies`, `films`, `music`) VALUES
-(121, 'admin', 'fsPZnsdpMIWNQ', 1, 'admin', 'admin@admin.com', '2019-05-05', '', 'c4e9ae3fcd89eeef986cef3f8e54bbaab751714e33e539ccb64d1d362ca6fece2217cdc2d19f38b8785f6dd54302ab23207c0de4ff50da12c3c3ca73f26f0c8aab0b310e56c08abc31fa6354ed0db0a006219a0030dcc12eabc5de0bb466b0bbba08', 'f38f8c16d4bce3e79a76980a657a296b02ce02e98a927fb8479f243e83847e767da80f52eef1cf77e7791b7fd8ff7e5935b15faf68de0ccbcce53d9c1daac0fca6629a94a957c46337f08a088e37434698ad623a914f39c50f9991282a8c35b1032887', 1, 2, NULL, NULL, '', '', NULL, NULL, NULL),
-(128, 'alfredo', 'fs9diRKEGnxgA', 0, 'Alfredo', 'alfredofauramolina@gmail.com', '1994-06-04', 'I like to ride the bike to work, swimming, and working out. I also like reading design magazines, go to museums, and binge watching a good tv show while it’s raining outside.', 'c4e9ae3fcd89eeef986cef3f8eef3954bbaab751714e33e539ccb64d1d362ca6fece2217cdc2d19f38b8785f6dd54302ab23207c0de4ff50da12c3c3ca73f26f0c8aab0b310e56c08abc31fa6354ed0db0a006219a0030dcc12eabc5de0bb466b0bbba08', 'f38f8c16d4bce3e79a76980a657a296b02ce02e98a927fb8479f243e83847e767da80f52eef1cf77e7791b7fd8ff7e5935b15faf68de0ccbcce53d9c1daac0fca6629a94a957c46337f08a088e37434698ad623a914f39c50f9991282a8c35b10328878a', 1, 2, '\\img\\128\\perfil.png', '\\img\\128\\cabecera.png', 'https://www.facebook.com/alfredo.faura', 'https://twitter.com/Alfredo_Faura', 'Prueba', 'Breaking Good, RedDevil, People of Interest, The Running Dead, Found,  American Guy.', 'Iron Maid, DC/AC, Megablow, The Ill, Kung Fighters, System of a Revenge.');
+INSERT INTO `user` (`id`, `username`, `password`, `rol`, `name`, `email`, `birthday`, `descripcion`, `authKey`, `accessToken`, `activate`, `centerCode`, `img_perfil`, `img_cabecera`, `facebook`, `twitter`, `hobbies`, `films`, `music`, `privado`) VALUES
+(121, 'admin', 'fsPZnsdpMIWNQ', 1, 'Alfredo Admin', 'admin@admin.com', '2019-05-05', '', 'c4e9ae3fcd89eeef986cef3f8e54bbaab751714e33e539ccb64d1d362ca6fece2217cdc2d19f38b8785f6dd54302ab23207c0de4ff50da12c3c3ca73f26f0c8aab0b310e56c08abc31fa6354ed0db0a006219a0030dcc12eabc5de0bb466b0bbba08', 'f38f8c16d4bce3e79a76980a657a296b02ce02e98a927fb8479f243e83847e767da80f52eef1cf77e7791b7fd8ff7e5935b15faf68de0ccbcce53d9c1daac0fca6629a94a957c46337f08a088e37434698ad623a914f39c50f9991282a8c35b1032887', 1, 2, '/img/121/15570547745ccec5360bcd07.59740587.png', '/img/121/15570547865ccec542b74f45.14487921.jpg', '', '', NULL, NULL, NULL, 1),
+(128, 'alfredo', 'fs9diRKEGnxgA', 0, 'Alfredo', 'alfredofauramolina@gmail.com', '1994-06-04', 'I like to ride the bike to work, swimming, and working out. I also like reading design magazines, go to museums, and binge watching a good tv show while it’s raining outside.', 'c4e9ae3fcd89eeef986cef3f8eef3954bbaab751714e33e539ccb64d1d362ca6fece2217cdc2d19f38b8785f6dd54302ab23207c0de4ff50da12c3c3ca73f26f0c8aab0b310e56c08abc31fa6354ed0db0a006219a0030dcc12eabc5de0bb466b0bbba08', 'f38f8c16d4bce3e79a76980a657a296b02ce02e98a927fb8479f243e83847e767da80f52eef1cf77e7791b7fd8ff7e5935b15faf68de0ccbcce53d9c1daac0fca6629a94a957c46337f08a088e37434698ad623a914f39c50f9991282a8c35b10328878a', 1, 2, '/img/128/15570468265ccea62a134c77.07140393.png', '/img/128/15570468315ccea62f830594.40415758.jpg', 'https://www.facebook.com/alfredo.faura', 'https://twitter.com/Alfredo_Faura', 'Prueba2', 'Breaking Good, RedDevil, People of Interest, The Running Dead, Found,  American Guy.', 'Iron Maid, DC/AC, Megablow, The Ill, Kung Fighters, System of a Revenge.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_block_other`
+--
+
+CREATE TABLE `user_block_other` (
+  `user_id` int(11) NOT NULL,
+  `blocked_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -124,6 +149,17 @@ INSERT INTO `user_has_friend` (`user_id`, `friend_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `user_has_request`
+--
+
+CREATE TABLE `user_has_request` (
+  `uid_ori` int(11) NOT NULL,
+  `uid_dest` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `user_has_tag`
 --
 
@@ -138,6 +174,7 @@ CREATE TABLE `user_has_tag` (
 
 INSERT INTO `user_has_tag` (`tag_id`, `user_id`) VALUES
 (2, 121),
+(5, 121),
 (5, 128);
 
 -- --------------------------------------------------------
@@ -156,7 +193,8 @@ CREATE TABLE `user_like_tag` (
 --
 
 INSERT INTO `user_like_tag` (`tag_id`, `user_id`) VALUES
-(2, 128);
+(5, 121),
+(5, 128);
 
 --
 -- Índices para tablas volcadas
@@ -167,6 +205,13 @@ INSERT INTO `user_like_tag` (`tag_id`, `user_id`) VALUES
 --
 ALTER TABLE `center`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indices de la tabla `tag`
@@ -183,10 +228,23 @@ ALTER TABLE `user`
   ADD KEY `fk_user_center_idx` (`centerCode`);
 
 --
+-- Indices de la tabla `user_block_other`
+--
+ALTER TABLE `user_block_other`
+  ADD PRIMARY KEY (`user_id`,`blocked_id`);
+
+--
 -- Indices de la tabla `user_has_friend`
 --
 ALTER TABLE `user_has_friend`
   ADD PRIMARY KEY (`user_id`,`friend_id`);
+
+--
+-- Indices de la tabla `user_has_request`
+--
+ALTER TABLE `user_has_request`
+  ADD PRIMARY KEY (`uid_ori`,`uid_dest`),
+  ADD KEY `uid_dest` (`uid_dest`);
 
 --
 -- Indices de la tabla `user_has_tag`
@@ -214,6 +272,12 @@ ALTER TABLE `center`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `tag`
 --
 ALTER TABLE `tag`
@@ -230,16 +294,34 @@ ALTER TABLE `user`
 --
 
 --
+-- Filtros para la tabla `fotos`
+--
+ALTER TABLE `fotos`
+  ADD CONSTRAINT `fotos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
 -- Filtros para la tabla `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `fk_user_center` FOREIGN KEY (`centerCode`) REFERENCES `center` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Filtros para la tabla `user_block_other`
+--
+ALTER TABLE `user_block_other`
+  ADD CONSTRAINT `user_block_other_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
 -- Filtros para la tabla `user_has_friend`
 --
 ALTER TABLE `user_has_friend`
   ADD CONSTRAINT `user_has_friend_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `user_has_request`
+--
+ALTER TABLE `user_has_request`
+  ADD CONSTRAINT `user_has_request_ibfk_1` FOREIGN KEY (`uid_dest`) REFERENCES `user` (`id`);
 
 --
 -- Filtros para la tabla `user_has_tag`
